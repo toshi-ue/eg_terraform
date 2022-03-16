b2dfdc5efc3f1eb1a19744428ba69ee881bdb634
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-03-16 21:22:56 +0900

    プライベートルートテーブルと関連付けを定義

--- practice/network.tf
+++ practice/network.tf
@@ -126,6 +126,19 @@ resource "aws_route_table_association" "public" {
 }
 
 /*
+　プライベートネットワーク用のルートテーブルを実装する。
+  インターネットゲートウェイに対するルーティング定義は不要。
+*/
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+/*
 プライベートネットワーク
 　インターネットから隔離されたネットワーク。
   データベースサーバーのような、インターネットからアクセスしないリソースを配置する。
