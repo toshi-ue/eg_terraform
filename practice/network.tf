5b170e8c14596e05cf8b0c97261db95ee951210a
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-03-17 14:21:35 +0900

    プライベートサブネットのルートテーブルのマルチAZ化

--- practice/network.tf
+++ practice/network.tf
@@ -151,15 +151,23 @@ resource "aws_route_table_association" "public_1" {
 　プライベートネットワーク用のルートテーブルを実装する。
   インターネットゲートウェイに対するルーティング定義は不要。
 */
-resource "aws_route_table" "private" {
+resource "aws_route_table" "private_0" {
   vpc_id = aws_vpc.example.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
 }
 
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
 /*
 プライベートネットワーク
 　インターネットから隔離されたネットワーク。
@@ -240,13 +248,18 @@ resource "aws_nat_gateway" "nat_gateway_1" {
   デフォルトルートをdestination_cidr_blockに指定し、
   NATゲートウェイにルーティングするよう設定する。
 */
-resource "aws_route" "private" {
-  route_table_id = aws_route_table.private.id
-  nat_gateway_id = aws_nat_gateway.example.id
+resource "aws_route" "private_0" {
+  route_table_id = aws_route_table.private_0.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
   # デフォルトルート（0.0.0.0/0）を設定し、NATゲートウェイにルーティングするよう設定する
   destination_cidr_block = "0.0.0.0/0"
 }
 
-
+resource "aws_route" "private_1" {
+  route_table_id = aws_route_table.private_1.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  # デフォルトルート（0.0.0.0/0）を設定し、NATゲートウェイにルーティングするよう設定する
+  destination_cidr_block = "0.0.0.0/0"
+}
 /*
 */
